a1213f0c32fdf5cfbb507a4f60c7a474b0d6ceb2
Ahmed <ahmed.hussein@zarcony.dev>
2023-06-24 04:03:56 +0300

    simple ruby variables

---
+++ variabls.rb
@@ -0,0 +1,16 @@
+character_name = "Mohamed"
+character_age = "60"
+puts "There once was a man named "+character_name
+puts "he was "+character_age+" years old"
+character_name = "Hussein"
+character_age = "26"
+puts "He really liked the name "+character_name
+puts "but didn't like being "+character_age
+
+
+name = "Ali"
+age = 75
+gpa = 3.2
+isTrue = true
+isFalse = false
+isEqualNull = nil
