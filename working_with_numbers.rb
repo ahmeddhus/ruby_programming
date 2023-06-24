04987e294b52f1556df775a0310bfd2225c90ac0
Ahmed <ahmed.hussein@zarcony.dev>
2023-06-24 04:29:44 +0300

    working with numbers

---
+++ working_with_numbers.rb
@@ -0,0 +1,17 @@
+puts 5
+puts 3.5
+puts -16
+puts 5 + 9
+puts 2**3
+
+
+num = 20
+puts "My fav num "+num.to_s  ## `.to_s` == toString()
+
+
+num = 20.8
+puts num.ceil()
+puts  num.floor()
+
+puts Math.sqrt(36)
+puts Math.log(1)
