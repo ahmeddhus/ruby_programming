cab0eb786d90b4aafe49894c76c675d041e30569
Ahmed <ahmed.hussein@zarcony.dev>
2023-06-24 04:22:35 +0300

    working with strings

---
+++ working_with_strings.rb
@@ -0,0 +1,19 @@
+phrase = "Giraffe Academy"
+puts phrase
+
+puts phrase.upcase()
+puts phrase.downcase()
+puts "ahmed".upcase()
+
+puts phrase.length()
+
+puts phrase.include? "Academy"
+
+puts phrase[0]+phrase[8]
+
+puts phrase[0,3]
+
+puts phrase.index("ffe")
+
+phrase = "    Giraffe Academy"
+puts phrase.strip()
