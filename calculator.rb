09fbe860000b7a1c956ab58cf125c2bdf5711613
Ahmed <ahmed.hussein@zarcony.dev>
2023-06-25 14:39:31 +0300

    simple calculator

---
+++ calculator.rb
@@ -0,0 +1,8 @@
+puts "Enter a number: "
+num1 = gets.chomp()
+
+puts "Enter another number"
+num2 = gets.chomp()
+
+puts num1.to_i + num2.to_i    # to_i converting string variable to integer.
+puts num1.to_f + num2.to_f    # to_f converting string varable to float.``
